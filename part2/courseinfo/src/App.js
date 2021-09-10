05a421b3e576921e439089ae6e9a97be5c0844b1
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2021-09-10 09:51:44 +0800

    [Phonebook] initialize all folders needed

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -56,40 +56,4 @@ const App = () => {
   );
 };
 
-// const Course = (props) => {
-//   const { course } = props;
-//   return (
-//     <div>
-//       <Header course={course}></Header>
-//       <Content course={course}></Content>
-//       <Total course={course}></Total>
-//     </div>
-//   );
-// };
-
-// const Header = (props) => {
-//   return <h2>{props.course.name}</h2>;
-// };
-
-// const Content = (props) => {
-//   return <Part parts={props.course.parts}></Part>;
-// };
-
-// const Total = (props) => {
-//   const total = props.course.parts.reduce((s, p) => {
-//     return s + p.exercises;
-//   }, 0);
-//   return <h4>total of {total} exercises</h4>;
-// };
-
-// const Part = (props) => {
-//   return props.parts.map((parts) => {
-//     return (
-//       <p key={parts.id}>
-//         {parts.name} {parts.exercises}
-//       </p>
-//     );
-//   });
-// };
-
 export default App;
