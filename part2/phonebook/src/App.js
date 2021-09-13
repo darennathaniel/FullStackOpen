b793751395fce243fae2803aaeebe5507188ac54
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2021-09-13 19:13:41 +0800

    [App] finished exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  function handleClick(e) {
+    e.preventDefault();
+    if (persons.filter((name) => name.name === newName).length > 0) {
+      window.alert(`${newName} is already added to phonebook`);
+    } else {
+      const newPerson = persons.concat({ name: newName, number: newNumber });
+      setPersons(newPerson);
+    }
+  }
+  function handleNameChange(e) {
+    setNewName(e.target.value);
+  }
+  function handleNumberChange(e) {
+    setNewNumber(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <p>
+        filter shown with<input></input>
+      </p>
+      <h2>add a new</h2>
+      <form>
+        <div>
+          name:{" "}
+          <input type="text" id="name" onChange={(e) => handleNameChange(e)} />
+        </div>
+        <div>
+          number:
+          <input
+            type="text"
+            id="number"
+            onChange={(e) => handleNumberChange(e)}
+          ></input>
+        </div>
+        <div>
+          <button type="submit" onClick={(e) => handleClick(e)}>
+            add
+          </button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => {
+        return (
+          <p>
+            {person.name} {person.number}
+          </p>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
