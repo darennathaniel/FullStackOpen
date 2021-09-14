1a89a2b1234c19f0c61cf79b710a8f6bf575382a
Daren Nathaniel Janto <67410689+darennathaniel@users.noreply.github.com>
2021-09-14 20:45:04 +0800

    [App] finished exercise 2.13

--- part2/dataforcountries/src/App.js
+++ part2/dataforcountries/src/App.js
@@ -4,6 +4,8 @@ import axios from "axios";
 const App = () => {
   const [data, setData] = useState([]);
   const [filterWord, setFilterWord] = useState("");
+  const [show, setShow] = useState(false);
+  const [index, setIndex] = useState(0);
   useEffect(() => {
     axios
       .get("https://restcountries.eu/rest/v2/all")
@@ -48,17 +50,72 @@ const App = () => {
                 {data.languages.map((language) => {
                   return <li>{language.name}</li>;
                 })}
-                <img src={data.flag} style={{ width: "25%" }}></img>
+                <img src={data.flag} style={{ width: "25%" }} alt="Flag"></img>
               </div>
             );
           })
+      ) : show ? (
+        <div>
+          <h1>
+            {
+              data.filter((word) =>
+                word.name.toLowerCase().includes(filterWord.toLowerCase())
+              )[index].name
+            }
+          </h1>
+          <p>
+            capital{" "}
+            {
+              data.filter((word) =>
+                word.name.toLowerCase().includes(filterWord.toLowerCase())
+              )[index].capital
+            }
+          </p>
+          <p>
+            population{" "}
+            {
+              data.filter((word) =>
+                word.name.toLowerCase().includes(filterWord.toLowerCase())
+              )[index].population
+            }
+          </p>
+          {data
+            .filter((word) =>
+              word.name.toLowerCase().includes(filterWord.toLowerCase())
+            )
+            [index].languages.map((language) => {
+              return <li>{language.name}</li>;
+            })}
+          <img
+            src={
+              data.filter((word) =>
+                word.name.toLowerCase().includes(filterWord.toLowerCase())
+              )[index].flag
+            }
+            style={{ width: "25%" }}
+            alt="Flag"
+          ></img>
+          <button onClick={() => setShow(false)}>close</button>
+        </div>
       ) : (
         data
           .filter((word) =>
             word.name.toLowerCase().includes(filterWord.toLowerCase())
           )
-          .map((data) => {
-            return <p key={parseInt(data.numericCode)}>{data.name}</p>;
+          .map((data, index) => {
+            return (
+              <div>
+                <p key={parseInt(data.numericCode)}>{data.name}</p>
+                <button
+                  onClick={() => {
+                    setShow(true);
+                    setIndex(index);
+                  }}
+                >
+                  show
+                </button>
+              </div>
+            );
           })
       )}
     </div>
